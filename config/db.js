f5e80747d74c3057adf5cf785a7384677cc21dea
AceyPe <Yahya.Adawy50@gmail.com>
2024-12-24 23:38:37 +0200

    commented db.js

--- config/db.js
+++ config/db.js
@@ -1,31 +1,31 @@
-import pg from 'pg'
-import dotenv from 'dotenv'
+// import pg from 'pg'
+// import dotenv from 'dotenv'
 
-const { Pool, Client } = pg
-dotenv.config();
+// const { Pool, Client } = pg
+// dotenv.config();
 
-export const pool = new Pool({
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    host: process.env.PGHOST,
-    port: process.env.PGPORT,
-    database: process.env.PGDATABASE,
-}
-)
+// export const pool = new Pool({
+//     user: process.env.PGUSER,
+//     password: process.env.PGPASSWORD,
+//     host: process.env.PGHOST,
+//     port: process.env.PGPORT,
+//     database: process.env.PGDATABASE,
+// }
+// )
 
-// console.log(pool)
-// console.log(await pool.query('SELECT user FROM USER'))
+// // console.log(pool)
+// // console.log(await pool.query('SELECT user FROM USER'))
 
-const client = new Client({
-    user: pool.user,
-    password: pool.password,
-    host: pool.host,
-    port: pool.port || 5432,
-    database: pool.database,
-})
+// const client = new Client({
+//     user: pool.user,
+//     password: pool.password,
+//     host: pool.host,
+//     port: pool.port || 5432,
+//     database: pool.database,
+// })
 
-await client.connect()
+// await client.connect()
 
-// console.log(await client.query('SELECT NOW()'))
+// // console.log(await client.query('SELECT NOW()'))
 
-await client.end()
+// await client.end()
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv'
-import { pool } from './config/db.js';
+// import { pool } from './config/db.js';
 
 dotenv.config();
 
